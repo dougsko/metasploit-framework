d9946c27168592790517c0c16fcf17e9988c1d7f
KarnGaneshen <KarnGaneshen@gmail.com>
2013-06-15 17:16:26 +0530

    added infovista module

--- modules/auxiliary/scanner/http/infovista_enum.rb
+++ modules/auxiliary/scanner/http/infovista_enum.rb
@@ -15,12 +15,19 @@ class Metasploit3 < Msf::Auxiliary
 	include Msf::Auxiliary::AuthBrute
 	include Msf::Auxiliary::Scanner
 
+	#
+	# CONSTANTS
+	# Used to check if remote app is InfoVista
+	#
+
+	INFOVISTA_FINGERPRINT = 'InfoVista® VistaPortal®'
+
 	def initialize(info={})
 		super(update_info(info,
 			'Name'           => 'InfoVista VistaPortal Application Brute Force Login Utility',
 			'Description'    => %{
-				This module attempts to scan for InfoVista VistaPortal Web Application, finds its
-			version and performs login brute force to identify valid credentials.
+				This module attempts to scan for InfoVista VistaPortal Web Application, finds its version
+			and performs login brute force to identify valid credentials.
 			},
 			'Author'         =>
 				[
@@ -39,14 +46,14 @@ class Metasploit3 < Msf::Auxiliary
 
 	def run_host(ip)
 		unless is_app_infovista?
-			print_error("#{rhost}:#{rport} - Application does not appear to be InfoVista VistaPortal. Module will not continue.")
+			print_error("#{rhost}:#{rport} -> Application does not appear to be InfoVista VistaPortal. Module will not continue.")
 			return
 		end
 
 		status = try_default_credential
 		return if status == :abort
 
-		print_status("#{rhost}:#{rport} - Brute-forcing...")
+		print_status("#{rhost}:#{rport} -> Brute-forcing...")
 		each_user_pass do |user, pass|
 			do_login(user, pass)
 		end
@@ -56,20 +63,21 @@ class Metasploit3 < Msf::Auxiliary
 	# What's the point of running this module if the app actually isn't InfoVista?
 	#
 	def is_app_infovista?
-		res = send_request_cgi(
-		{
-			'uri'       => '/VPortal/',
-			'method'    => 'GET'
-		})
 
-		if (res and res.code == 200 and res.body =~ /InfoVista.*VistaPortal/)
-			version_key = /PORTAL_VERSION = (.+)./
-			version = res.body.scan(version_key).flatten[0].gsub('"','')
-			print_good("#{rhost}:#{rport} - Application version is #{version}")
-			return true
-		else
-			return false
-		end
+			res = send_request_cgi(
+			{
+				'uri'       => '/VPortal/',
+				'method'    => 'GET'
+			})
+
+			if (res and res.code == 200 and res.body.include?(INFOVISTA_FINGERPRINT))
+				version_key = /PORTAL_VERSION = (.+)./
+				version = res.body.scan(version_key).flatten[0].gsub('"','')
+				print_good("#{rhost}:#{rport} -> Application version is #{version}")
+				return true
+			else
+				return false
+			end
 	end
 
 	#
@@ -85,11 +93,11 @@ class Metasploit3 < Msf::Auxiliary
 	# Brute-force the login page
 	#
 	def do_login(user, pass)
-		vprint_status("#{rhost}:#{rport} - Trying username:#{user.inspect} with password:#{pass.inspect}")
+		vprint_status("#{rhost}:#{rport} -> Trying username:#{user.inspect} with password:#{pass.inspect}")
 		begin
 			res = send_request_cgi(
 			{
-				'uri'       => target_uri.to_s,
+				'uri'       => '/VPortal/mgtconsole/CheckPassword.jsp',
 				'method'    => 'POST',
 				'vars_post' =>
 					{
@@ -98,10 +106,13 @@ class Metasploit3 < Msf::Auxiliary
 					}
 			})
 
-			if (not res or res.code != 200 or res.body !~ /location.href.*AdminFrame\.jsp/)
-				vprint_error("#{rhost}:#{rport} - FAILED LOGIN - #{user.inspect}:#{pass.inspect} with code #{res.code}")
+			get_response = "<script type=\"text/javascript\">\r\nlocation.href = 'AdminFrame.jsp';\r\n</script>\r\n"
+
+			if (not res or res.code != 200 and res.body != "#{get_response}")
+				vprint_error("#{rhost}:#{rport} -> FAILED LOGIN - #{user.inspect}:#{pass.inspect} with code #{res.code}")
+				return :skip_pass
 			else
-				print_good("#{rhost}:#{rport} - SUCCESSFUL LOGIN - #{user.inspect}:#{pass.inspect}")
+				print_good("#{rhost}:#{rport} -> SUCCESSFUL LOGIN - #{user.inspect}:#{pass.inspect}")
 
 				report_hash = {
 					:host   => rhost,
@@ -110,15 +121,14 @@ class Metasploit3 < Msf::Auxiliary
 					:user   => user,
 					:pass   => pass,
 					:active => true,
-					:type => 'password'
-				}
+					:type => 'password'}
 
 				report_auth_info(report_hash)
 				return :next_user
 			end
 
 		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError, ::Errno::EPIPE
-			print_error("#{rhost}:#{rport} - HTTP Connection Failed, Aborting")
+			print_error("#{rhost}:#{rport} -> HTTP Connection Failed, Aborting")
 			return :abort
 		end
 	end
